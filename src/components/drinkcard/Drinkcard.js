66758cb5192b896e17ec225b6b7601c56947f1c7
unknown <hrushikesh7702@gmail.com>
2024-06-10 01:02:12 +0530

    first commit

--- src/components/drinkcard/Drinkcard.js
+++ src/components/drinkcard/Drinkcard.js
@@ -2,9 +2,12 @@ import React from 'react'
 import "./Drinkcard.css"
 import { Link } from 'react-router-dom'
 function Drinkcard({props}) {
-  
+  const handleClick = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
-    <a href={`/recipe/${props.idDrink}`} style={{ textDecoration: 'none' }}>
+    <Link to={`/recipe/${props.idDrink}`} style={{ textDecoration: 'none' }} onClick={handleClick}>
     <div className="col card-container ">
         <div className="card h-100 w-auto border border-0 rounded-0 buttoncard">
           <img src={props.strDrinkThumb} className="card-img-top h-100 d-inline-block rounded-0" alt="..." />
@@ -13,7 +16,7 @@ function Drinkcard({props}) {
           </div>
         </div>
       </div>
-  </a>
+  </Link>
   )
 }
 
